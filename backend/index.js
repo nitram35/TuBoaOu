6a7c76165e0a8ee9a5b775b62329c0e304fde5f0
Martin Gandon <martin.gandon@utt.fr>
2024-06-06 21:02:03 +0200

    connect to the mongoDB database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const app = express()
+dotenv.config();
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB!');
+    })
+    .catch(() => {
+        console.log('Connection failed!');
+    });
+
+const app = express();
 
 app.listen(3000, () => {
-    console.log('Our server is running on http://localhost:3000 !');
+    console.log('Our server is running on http://localhost:3000');
 });
