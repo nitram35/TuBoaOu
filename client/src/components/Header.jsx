6366e9cf9df707f2770f0d55edd780e8fd45ccb4
Martin Gandon <martin.gandon@utt.fr>
2024-06-08 17:45:14 +0200

    adding group page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import { Navbar, TextInput, Button } from 'flowbite-react'
 import { Link, useLocation } from 'react-router-dom'
 import { AiOutlineSearch } from 'react-icons/ai'
-import { FaMoon } from 'react-icons/fa'
 
 export default function Header() {
     const path = useLocation().pathname;
     return (
         <Navbar className='border-b-2'>
             <Link to='/' className='self-centered whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-                <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>TuBoaOu</span>
+                <span className='px-2 py-1 bg-gradient-to-r from-green-400 to-blue-500 rounded-lg text-white'>TuBoaOu</span>
             </Link>
             <form>
                 <TextInput
@@ -22,11 +21,8 @@ export default function Header() {
                 <AiOutlineSearch />
             </Button>
             <div className="flex gap-2 md:order-2">
-                <Button className='w-12 h-10 hidden sm:inline' color='gray'>
-                    <FaMoon />
-                </Button>
                 <Link to='/sign-in'>
-                    <Button gradientDuoTone='purpleToBlue' outline>
+                    <Button gradientDuoTone='greenToBlue' outline>
                         Sign In
                     </Button>
                 </Link>
@@ -38,9 +34,9 @@ export default function Header() {
                         Home
                     </Link>
                 </Navbar.Link>
-                <Navbar.Link active={path === '/about'} as={'div'}>
-                    <Link to='/about'>
-                        About
+                <Navbar.Link active={path === '/group'} as={'div'}>
+                    <Link to='/group'>
+                        Group
                     </Link>
                 </Navbar.Link>
                 <Navbar.Link active={path === '/sign-in'} as={'div'}>
