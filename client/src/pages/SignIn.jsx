0462e97c4f7803745dc397cc1c260b03297ca636
Martin Gandon <martin.gandon@utt.fr>
2024-06-10 17:05:14 +0200

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,15 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { signInStart, signInSucceeded, signInFailed } from "../redux/user/userSlice";
 
 export default function SignIn() {
     const [formData, setFormData] = useState({});
-    const [errorMessage, setErrorMessage] = useState(null);
-    const [loading, setLoading] = useState(false);
+    // const [errorMessage, setErrorMessage] = useState(null);
+    // const [loading, setLoading] = useState(false);
+    const { loading, error: errorMessage } = useSelector((state) => state.user);
+    const dispatch = useDispatch();
     const navigate = useNavigate();
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
@@ -13,11 +17,14 @@ export default function SignIn() {
     const handleSubmit = async (e) => {
         e.preventDefault(); // prevent the default form submission
         if (!formData.email || !formData.password) {
-            return setErrorMessage('All fields are required');
+            // return setErrorMessage('All fields are required');
+            return dispatch(signInFailed('All fields are required'));
         }
         try {
-            setLoading(true);
-            setErrorMessage(null);
+            // We replace the 2 following lines with Redux by using the signInStart
+            // setLoading(true);
+            // setErrorMessage(null);
+            dispatch(signInStart());
             // we add a proxy in vite.config.js to be able to reach localhost:3000 (server) and not localhost:5173 (client)
             const res = await fetch('/api/auth/signin', {
                 method: 'POST',
@@ -28,16 +35,19 @@ export default function SignIn() {
             });
             const data = await res.json();
             if (data.success === false) {
-                setErrorMessage(data.message);
+                // setErrorMessage(data.message);
+                dispatch(signInFailed(data.message));
+
             }
-            setLoading(false);
             // if the response is ok, we redirect to the sign-in pages
             if (res.ok) {
+                dispatch(signInSucceeded(data));
                 navigate('/');
             }
         } catch (error) {
-            setErrorMessage(error.message)
-            setLoading(false);
+            // setErrorMessage(error.message)
+            // setLoading(false);
+            dispatch(signInFailed(error.message));
         }
     };
     console.log(formData);
