8e486c183ad8a8b8c68ceb214c4207644734adb5
Martin Gandon <martin.gandon@utt.fr>
2024-06-10 16:25:20 +0200

    sign in api route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     // console.log(req.body);
@@ -32,5 +33,40 @@ export const signup = async (req, res, next) => {
         next(error);
     }
 
+
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || email === '' || !password || password === '') {
+        next(errorHandler(400, 'All fields are required'));
+    }
+
+    try {
+        // findOne is going to search for email in the database
+        const validUser = await User.findOne({ email });
+        // If there is no user with that email
+        if (!validUser) {
+            return next(errorHandler(404, 'User not found'));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(400, 'Invalid password'));
+        }
+        // If everything is correct we are going to create a token for the user
+        const token = jwt.sign(
+            { userId: validUser._id },
+            process.env.JWT_SECRET
+        );
+        // We don't want to send the password back even if it's hashed
+        const { password: pass, ...rest } = validUser._doc;
+
+        // We create a cookie with the token
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true
+        }).json(rest);
+    } catch (error) {
+        next(error);
+    }
 
 };
