72defbdd69209cea012ddb82302b4cf8ab5799e2
Martin Gandon <martin.gandon@utt.fr>
2024-06-10 19:34:23 +0200

    profile page UI

--- client/src/components/DashboardProfile.jsx
+++ client/src/components/DashboardProfile.jsx
@@ -1,5 +1,26 @@
+import { Button, TextInput } from 'flowbite-react';
+import { useSelector } from 'react-redux';
+
 export default function DashboardProfile() {
+    const { currentUser } = useSelector(state => state.user);
     return (
-        <div>DashboardProfile</div>
+        <div className='max-w-lg mx-auto p-3 w-full'>
+            <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
+            <form className='flex flex-col gap-4'>
+                <div className='w-30 h-30 self-center'>
+                    <img src={currentUser.profileAvatar} alt="user" className='rounded-full w-full h-full' />
+                </div>
+                <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username} />
+                <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} />
+                <TextInput type='password' id='password' placeholder='password' />
+                <Button type='submit' gradientDuoTone='greenToBlue' outline>
+                    Update
+                </Button>
+            </form>
+            <div className='text-red-600 flex justify-between mt-5'>
+                <span className='cursor-pointer'>Delete Account</span>
+                <span className='cursor-pointer'>Sign Out</span>
+            </div>
+        </div>
     )
 }
