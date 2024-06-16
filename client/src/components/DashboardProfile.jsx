f2e4a6d78b8891fd9cb827dba33ab42f0baf708f
Martin Gandon <martin.gandon@utt.fr>
2024-06-16 16:10:25 +0200

    adding adddress submiting with Google Places API autocomplete + retrieving coordinates of the address

--- client/src/components/DashboardProfile.jsx
+++ client/src/components/DashboardProfile.jsx
@@ -1,33 +1,71 @@
 import { Alert, Button, Modal, TextInput } from 'flowbite-react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useState } from 'react';
-import { updateStart, updateSucceeded, updateFailed, deleteUserStart, deleteUserSucceeded, deleteUserFailed, signoutSucceeded } from '../redux/user/userSlice';
+import { useState, useRef, useEffect } from 'react';
+import {
+    updateStart,
+    updateSucceeded,
+    updateFailed,
+    deleteUserStart,
+    deleteUserSucceeded,
+    deleteUserFailed,
+    signoutSucceeded
+} from '../redux/user/userSlice';
+
+// Function to load Google Maps script asynchronously
+const loadGoogleMapsScript = (callback) => {
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_API_KEY}&libraries=places`;
+    script.async = true;
+    script.onload = callback;
+    document.body.appendChild(script);
+};
 
 export default function DashboardProfile() {
     const { currentUser, error } = useSelector(state => state.user);
+    const dispatch = useDispatch();
+    const addressInputRef = useRef(null);
     const [formData, setFormData] = useState({});
     const [updateUserSucceeded, setUpdateUserSucceeded] = useState(null);
     const [updateUserError, setUpdateUserError] = useState(null);
     const [showModal, setShowModal] = useState(false);
-    const dispatch = useDispatch();
+
+    useEffect(() => {
+        loadGoogleMapsScript(() => {
+            const autocomplete = new window.google.maps.places.Autocomplete(addressInputRef.current, {
+                types: ['address'],
+            });
+            autocomplete.addListener('place_changed', () => {
+                const place = autocomplete.getPlace();
+                setFormData(prev => ({
+                    ...prev,
+                    address: place.formatted_address,
+                    latitude: place.geometry.location.lat(),
+                    longitude: place.geometry.location.lng()
+                }));
+            });
+        });
+    }, []);
 
     const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.id]: e.target.value });
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value
+        });
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        // reset the alert messages
         setUpdateUserError(null);
         setUpdateUserSucceeded(null);
-        // if no data is changed, return
+
         if (Object.keys(formData).length === 0) {
             setUpdateUserError("No data updated!");
             return;
         }
+
         try {
             dispatch(updateStart());
-            // make a request to the server
+
             const response = await fetch(`/api/user/update/${currentUser._id}`, {
                 method: 'PUT',
                 headers: {
@@ -35,24 +73,20 @@ export default function DashboardProfile() {
                 },
                 body: JSON.stringify(formData),
             });
+
             const data = await response.json();
             if (!response.ok) {
                 dispatch(updateFailed(data.message));
                 setUpdateUserError(data.message);
-
             } else {
                 dispatch(updateSucceeded(data));
                 setUpdateUserSucceeded("Profile updated successfully!");
             }
         } catch (error) {
-            // dispatch(updateFailed(error.message));
+            dispatch(updateFailed(error.message));
             setUpdateUserError(error.message);
-
         }
-
     };
-    // console.log(currentUser);
-    // console.log(formData);
 
     const handleDeleteUser = async () => {
         setShowModal(false);
@@ -71,6 +105,7 @@ export default function DashboardProfile() {
             dispatch(deleteUserFailed(error.message));
         }
     };
+
     const handleSignout = async () => {
         try {
             const response = await fetch('/api/user/signout', {
@@ -86,17 +121,18 @@ export default function DashboardProfile() {
             console.log(error.message);
         }
     };
+
     return (
         <div className='max-w-lg mx-auto p-3 w-full'>
             <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
-            <form onSubmit={handleSubmit} className='flex flex-col gap-4' >
+            <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
                 <div className='w-30 h-30 self-center'>
                     <img src={currentUser.profileAvatar} alt="user" className='rounded-full w-full h-full' />
                 </div>
                 <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username} onChange={handleChange} />
                 <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} onChange={handleChange} />
-                <TextInput type='password' id='password' placeholder='password' defaultValue={currentUser.password} onChange={handleChange} />
-                <TextInput type='text' id='address' placeholder='address' defaultValue={currentUser.address} onChange={handleChange} />
+                <TextInput type='password' id='password' placeholder='password' onChange={handleChange} />
+                <TextInput type='text' id='address' placeholder='address' defaultValue={currentUser.address} ref={addressInputRef} />
                 <Button type='submit' gradientDuoTone='greenToBlue' outline>
                     Update
                 </Button>
@@ -121,5 +157,5 @@ export default function DashboardProfile() {
                 </Modal.Body>
             </Modal>
         </div>
-    )
+    );
 }
