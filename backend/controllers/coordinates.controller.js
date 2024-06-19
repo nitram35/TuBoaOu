27c28eed90a8757851ff727c6f44d5dd42b36fea
Martin Gandon <martin.gandon@utt.fr>
2024-06-19 12:32:58 +0200

    more generic

--- backend/controllers/coordinates.controller.js
+++ backend/controllers/coordinates.controller.js
@@ -4,13 +4,19 @@ import { errorHandler } from '../utils/error.js';
 export const getMeanCoordinates = async (req, res, next) => {
     try {
         const { users } = req.body;
+        console.log(users);
 
         // Validate input
         if (!Array.isArray(users) || users.length === 0) {
             return next(errorHandler(400, 'Invalid users array'));
         }
 
-        const meanCoordinates = calculateMeanCoordinates(users);
+        // Specify key names for longitude and latitude
+        const longitudeKey = 'longitude';
+        const latitudeKey = 'latitude';
+
+        // Calculate mean coordinates using the refactored function
+        const meanCoordinates = calculateMeanCoordinates(users, longitudeKey, latitudeKey);
 
         res.status(200).json(meanCoordinates);
     } catch (error) {
